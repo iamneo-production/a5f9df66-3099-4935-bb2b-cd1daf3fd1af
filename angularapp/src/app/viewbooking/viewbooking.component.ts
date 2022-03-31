30d423021ba388b8bc40bff46ed925dd018ebd1b
arun16072001 <madhavanarun17@gmail.com>
2022-03-31 19:36:30 +0530

    viewbookingadded

--- angularapp/src/app/viewbooking/viewbooking.component.ts
+++ angularapp/src/app/viewbooking/viewbooking.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { BookeventService } from '../bookevent.service';
+import { LoginService } from '../login.service';
 
-@Component({
+@Component({ 
   selector: 'app-viewbooking',
   templateUrl: './viewbooking.component.html',
   styleUrls: ['./viewbooking.component.css']
 })
 export class ViewbookingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _loginService:LoginService, private _bookEvent: BookeventService) { }
+
+  events:any;
 
   ngOnInit(): void {
+    this._bookEvent.getByEmail(this._loginService.User.email).subscribe((data)=>{
+      console.log(data);
+      this.events=data;
+    })
+  }
+
+  logout()
+  {
+    this._loginService.Logout();
   }
 
 }
