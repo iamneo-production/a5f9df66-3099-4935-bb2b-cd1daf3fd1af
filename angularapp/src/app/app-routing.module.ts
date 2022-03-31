1cad537dc1dc8581285eec443a4a8c25b7d1d1fe
karthikeyan k <k.karthikeyankcbe@gmail.com>
2022-03-31 20:38:35 +0530

    app

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthguardGuard } from './authguard.guard';
+import { BookingComponent } from './booking/booking.component';
+import { DisplayUserComponent } from './display-user/display-user.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
+import { RegistrationsuccessComponent } from './registrationsuccess/registrationsuccess.component';
 
-const routes: Routes = [];
+import { ViewbookingComponent } from './viewbooking/viewbooking.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'registration', component: RegisterpageComponent },
+  {path: 'registrationsuccess', component: RegistrationsuccessComponent },
+  {path: 'userhomepage', component: HomepageComponent , canActivate:[AuthguardGuard]},
+  {path: 'viewbooking', component: ViewbookingComponent , canActivate:[AuthguardGuard]},
+  {path: 'displayuer',component:DisplayUserComponent,canActivate:[AuthguardGuard]},
+  {path:'bookevent',component:BookingComponent,canActivate:[AuthguardGuard]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+}) 
 export class AppRoutingModule { }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,24 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';;
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationsuccessComponent } from './registrationsuccess/registrationsuccess.component';
-import { BookingComponent } from './booking/booking.component';
-import { HomepageComponent } from './homepage/homepage.component';
 import { RegisterpageComponent } from './registerpage/registerpage.component';
+import { HttpClientModule} from '@angular/common/http';
+import { RegistrationsuccessComponent } from './registrationsuccess/registrationsuccess.component';
+import { LoginComponent } from './login/login.component';
+import { BookingComponent } from './booking/booking.component';
+import { ViewbookingComponent } from './viewbooking/viewbooking.component';
+import { DisplayUserComponent } from './display-user/display-user.component';
+import { HomepageComponent } from './homepage/homepage.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegisterpageComponent,
     RegistrationsuccessComponent,
+    LoginComponent,
     BookingComponent,
+    ViewbookingComponent,
+    DisplayUserComponent,
     HomepageComponent,
-    RegisterpageComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
