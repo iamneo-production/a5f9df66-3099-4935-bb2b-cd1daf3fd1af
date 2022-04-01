040e533ef5bbf6ac7760dc3875883941f27c32d9
Sankalita04 <sankalita.chakrabortty@gmail.com>
2022-04-01 12:10:04 +0530

    added addthemeclass

---
+++ angularapp/src/app/add-theme.spec.ts
@@ -0,0 +1,7 @@
+import { AddTheme } from './add-theme';
+
+describe('AddTheme', () => {
+  it('should create an instance', () => {
+    expect(new AddTheme()).toBeTruthy();
+  });
+});
---
+++ angularapp/src/app/add-theme.ts
@@ -0,0 +1,10 @@
+export class AddTheme {
+    themeName!:string;
+    imageUrl!:string;
+    photographerDetails!:string;
+    videographerDetails!:string;
+    returnGift!:string;
+    themeCost!:string;
+    themeDiscription!:string;
+}
+
